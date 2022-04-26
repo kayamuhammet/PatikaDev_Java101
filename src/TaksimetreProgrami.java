689ea6bc6fd3dc24ef694a4eac6a291fa56d3f7c
kayamuhammet <kayamuhammet78@gmail.com>
2022-04-26 03:44:14 +0300

    Taksimetre hesaplama programi.

---
+++ src/TaksimetreProgrami.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class TaksimetreProgrami {
+    public static void main(String[] args) {
+        int acilis = 10;
+        double kmBasina = 2.20, km, tutar;
+        Scanner input = new Scanner(System.in);
+        System.out.print("Lutfen Gidilen Mesafeyi(KM cinsinden) Yaziniz : ");
+        km = input.nextDouble();
+        tutar = acilis + (km * kmBasina);
+        if (tutar<20){
+            System.out.println("Odenecek Tutar : " + 20 + " TL");
+        }else{
+            System.out.println("Odenecek Tutar : " + tutar + " TL");
+        }
+
+    }
+}
